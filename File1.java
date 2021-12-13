bbc0aed8e2a374aadf4ffb9aa8a2ed96811cf995
Natalya Talalova <natalyat@axosoft.com>
2021-12-13 10:12:50 +0600

    AAA-1

---
+++ File1.java
@@ -0,0 +1,17 @@
+package ru.java.sandbox;l;l
+
+public class MyFirstProgram {
+ public static void main (String[] args) {
+   System.out.print("Hello, world");
+   }
+}p[
+hg
+gfhfg
+fhfghfgdjj
+kjj
+ljhjjhjk2
+рррав
+jhghgjhjhg
+jjfdfu
+rerejkhjhjh
+kk
